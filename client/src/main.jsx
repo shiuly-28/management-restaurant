ed9e97207d91d8c387ecb0c4cfe6d39bb767f80c
shiuly-28 <shulybd1245@gmail.com>
2026-02-16 23:45:38 +0600

    lenies added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,13 +6,18 @@ import router from './router/router.jsx'
 import { RouterProvider } from 'react-router-dom'
 import AuthProvider from './context/AuthProvider.jsx'
 import { ToastContainer } from 'react-toastify'
+import LenisProvider from './provider/LeniesProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ToastContainer />
     <AuthProvider>
+      <LenisProvider>
+
       <RouterProvider router={router} />
+      </LenisProvider>
     </AuthProvider>
+
   </StrictMode>,
 )
 
